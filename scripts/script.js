49700f69c0300735ab94096202704c42a77a753e
Kareem Moataz <kareemoataz13@gmail.com>
2022-08-15 22:59:45 +0200

    push

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
-const lightTheme = "styles/light.css";
 const darkTheme = "styles/dark.css";
-const sunIcon = "assets/SunIcon.svg";
 const moonIcon = "assets/MoonIcon.svg";
 const themeIcon = document.getElementById("theme-icon");
 const res = document.getElementById("result");
@@ -18,24 +16,7 @@ function calculate(value) {
   }
 }
 
-// Swaps the stylesheet to achieve dark mode.
-function changeTheme() {
-  const theme = document.getElementById("theme");
-  setTimeout(() => {
-    toast.innerHTML = "Calculator";
-  }, 1500);
-  if (theme.getAttribute("href") === lightTheme) {
-    theme.setAttribute("href", darkTheme);
-    themeIcon.setAttribute("src", sunIcon);
-    toast.innerHTML = "Dark Mode 🌙";
-  } else {
-    theme.setAttribute("href", lightTheme);
-    themeIcon.setAttribute("src", moonIcon);
-    toast.innerHTML = "Light Mode ☀️";
-  }
-}
 
-// Displays entered value on screen.
 function liveScreen(enteredValue) {
   if (!res.value) {
     res.value = "";
@@ -43,15 +24,15 @@ function liveScreen(enteredValue) {
   res.value += enteredValue;
 }
 
-//adding event handler on the document to handle keyboard inputs
+
 document.addEventListener("keydown", keyboardInputHandler);
 
-//function to handle keyboard inputs
+
 function keyboardInputHandler(e) {
-  // to fix the default behavior of browser,
-  // enter and backspace were causing undesired behavior when some key was already in focus.
+
+
   e.preventDefault();
-  //grabbing the liveScreen
+
 
   //numbers
   if (e.key === "0") {
@@ -94,12 +75,11 @@ function keyboardInputHandler(e) {
     res.value += ".";
   }
 
-  //press enter to see result
+
   if (e.key === "Enter") {
     calculate(result.value);
   }
 
-  //backspace for removing the last input
   if (e.key === "Backspace") {
     const resultInput = res.value;
     //remove the last element in the string
